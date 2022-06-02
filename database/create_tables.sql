9d3c5fae445377eaade61883a49dd9067051c5a3
Pedro Ribeiro <pedrouminhoribeiro@gmail.com>
2022-06-02 13:28:56 +0100

    :card_file_box: Updates the initial database setup with different rules

--- database/create_tables.sql
+++ database/create_tables.sql
@@ -20,11 +20,12 @@ CREATE TABLE parameters (
     CONSTRAINT fk_rule FOREIGN KEY(rule_token) REFERENCES rules(token)
 );
 
-INSERT INTO rules VALUES (1, 'gt 1.0.0', null, null, null, null, null, null, null, 'template_1.yml', 1);
+INSERT INTO rules VALUES (1, 'gt 1.0.0 and lt 3.0.0', null, null, null, null, null, null, null, 'template_1.yml', 1);
 INSERT INTO rules VALUES (2, null, 'eq 192.168.1.6', 'OLT2T2', null, null, null, null, null, 'template_2.yml', 1);
 INSERT INTO rules VALUES (3, null, null, 'OLT2T2', 'CARD-TYPE-1', null, null, null, null, 'template_3.yml', 2);
 INSERT INTO rules VALUES (4, 'gt 1.0.0', 'startswith 192.168', null, null, null, null, null, null, 'template_4.yml', 2);
-INSERT INTO rules VALUES (5, 'lt 5.0.0', 'endswith 1.6', null, null, null, null, null, null, 'template_5.yml', 3);
+INSERT INTO rules VALUES (5, null, 'startswith 192 and endswith 8', null, null, null, null, null, null, 'template_4.yml', 2);
+INSERT INTO rules VALUES (6, 'lt 5.0.0', 'endswith 1.6', null, null, null, null, null, null, 'template_5.yml', 3);
 
-CREATE SEQUENCE rule_id_seq START 6;
+CREATE SEQUENCE rule_id_seq START 7;
 CREATE SEQUENCE parameter_id_seq START 1;
